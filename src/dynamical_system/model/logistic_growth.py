7ae36df6eaa4c1d0489d2ffd1fef9daeea50a32a
iChemy <controlyuu.dev@gmail.com>
2024-04-26 18:21:05 +0900

    :zap: add model module

---
+++ src/dynamical_system/model/logistic_growth.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+class DiscreteLogisticGrowth:
+    def __init__(self, a: float):
+        self._a = a
+
+    def model(self, x: float | np.ndarray):
+        return self._a * x * (1 - x)
+
+    def simulate(self, inits: float | list[float], steps: int):
+        if isinstance(inits, float):
+            inits = [inits]
+
+        X = np.array([[x] for x in inits])
+
+        for _ in range(steps):
+            x = self.model(X[:, -1:])
+            X = np.concatenate((X, x), axis=1)
+        return X
+
+    def web_simulate(
+        self,
+        init: float,
+        steps: int,
+        web_y_init: float = 0.0,
+        x_range: np.ndarray = np.linspace(0, 1, 100),
+    ):
+        Web_x = np.array([init])
+        Web_y = np.array([web_y_init])
+
+        for _ in range(steps):
+            x = self.model(Web_x[-1])
+            Web_x = np.append(Web_x, [Web_x[-1], x])
+            Web_y = np.append(Web_y, [x, x])
+
+        y_model_map = self.model(x_range)
+
+        return Web_x, Web_y, y_model_map, x_range
